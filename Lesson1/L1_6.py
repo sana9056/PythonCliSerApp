a3fb3bcbb8bc5fa2dd486523afbb36404b5c7a07
Sana9056 <sana9056@mail.ru>
2022-04-21 20:25:16 +0300

    Доделки

--- Lesson1/L1_6.py
+++ Lesson1/L1_6.py
@@ -2,14 +2,27 @@
 программирование», «сокет», «декоратор». Проверить кодировку файла по умолчанию.
 Принудительно открыть файл в формате Unicode и вывести его содержимое.'''
 
-file = open("test_file1.txt", "w")
-file.write("сетевое программирование\n")
-file.write("сокет\n")
-file.write("декоратор\n")
-# кодировка ФАЙЛА по умолчанию
-print(file.encoding)
+from chardet import detect
+
+LIST = ['сетевое программирование', 'сокет', 'декоратор']
+with open('test_file.txt', 'w') as file:
+    for line in LIST:
+        file.write(f'{line}\n')
 file.close()
 
-with open("test_file.txt", encoding='utf-8') as file:
-    for line in file:
-        print(line, end='')
+
+def encoding_convert():
+    with open('test.txt', 'rb') as f_obj:
+        content_bytes = f_obj.read()
+    detected = detect(content_bytes)
+    encoding = detected['encoding']
+    content_text = content_bytes.decode(encoding)
+    with open('test.txt', 'w', encoding='utf-8') as f_obj:
+        f_obj.write(content_text)
+
+
+encoding_convert()
+
+with open('test.txt', 'r', encoding='utf-8') as file:
+    CONTENT = file.read()
+print(CONTENT)
