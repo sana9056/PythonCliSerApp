74e4d1edb455dd12d7b3eaff9ea2a2f4e6105871
Sana9056 <sana9056@mail.ru>
2022-05-02 08:48:06 +0300

    HomeW5

--- Lesson3/client.py
+++ Lesson3/client.py
@@ -2,9 +2,15 @@ import sys
 import json
 import socket
 import time
+import argparse
+import logging
+import log.client_log_config
 from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
-    RESPONSE, ERROR, DEFAULT_IP_ADDRESS, DEFAULT_PORT
+    RESPONSE, DEFAULT_PORT, ERROR, DEFAULT_IP_ADDRESS
 from common.utils import get_message, send_message
+
+
+CLIENT_LOGGER = logging.getLogger('client')
 
 
 def create_presence(account_name='Guest'):
@@ -15,36 +21,51 @@ def create_presence(account_name='Guest'):
             ACCOUNT_NAME: account_name
         }
     }
+    CLIENT_LOGGER.debug(f'Сформировано {PRESENCE} сообщение для пользователя {account_name}')
     return out
 
 
 def process_ans(message):
+    CLIENT_LOGGER.debug(f'Разбор сообщения от сервера: {message}')
     if RESPONSE in message:
         if message[RESPONSE] == 200:
             return '200 : ACCEPTED'
         return f'400 : {message[ERROR]}'
     raise ValueError
 
+def create_arg_parser():
+    """
+    Создаём парсер аргументов коммандной строки
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('addr', default=DEFAULT_IP_ADDRESS, nargs='?')
+    parser.add_argument('port', default=DEFAULT_PORT, type=int, nargs='?')
+    return parser
+
 
 def main():
-    try:
-        server_address = sys.argv[2]
-        server_port = int(sys.argv[3])
-        if server_port < 1024 or server_port > 65535:
-            raise ValueError
-    except IndexError:
-        server_address = DEFAULT_IP_ADDRESS
-        server_port = DEFAULT_PORT
-    except ValueError:
-        print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
+    parser = create_arg_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    server_address = namespace.addr
+    server_port = namespace.port
+
+    if not 1023 < server_port < 65536:
+        CLIENT_LOGGER.critical(
+            f'Попытка запуска клиента с неподходящим номером порта: {server_port}.'
+            f' Допустимы адреса с 1024 до 65535. Клиент завершается.')
         sys.exit(1)
 
-    transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.connect((server_address, server_port))
-    message_to_server = create_presence()
-    send_message(transport, message_to_server)
+    CLIENT_LOGGER.info(f'Запущен клиент с парамертами: '
+                       f'адрес сервера: {server_address}, порт: {server_port}')
+
     try:
+        transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        transport.connect((server_address, server_port))
+        message_to_server = create_presence()
+        send_message(transport, message_to_server)
         answer = process_ans(get_message(transport))
+        CLIENT_LOGGER.info(f'Принят ответ от сервера {answer}')
         print(answer)
     except (ValueError, json.JSONDecodeError):
         print('Не удалось декодировать сообщение сервера.')
--- Lesson3/server.py
+++ Lesson3/server.py
@@ -1,60 +1,71 @@
 import socket
 import sys
+import argparse
 import json
-from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTIONS, \
-    PRESENCE, TIME, USER, ERROR, DEFAULT_PORT
+import logging
+import log.server_log_config
+from common.variables import ACTION, PRESENCE, TIME, USER, ACCOUNT_NAME, \
+    RESPONSE, DEFAULT_PORT, MAX_CONNECTIONS, ERROR
 from common.utils import get_message, send_message
 
 
-def message_response(message):
-    if ACTION in message and message[ACTION] == PRESENCE and TIME in message and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+SERVER_LOGGER = logging.getLogger('server')
+
+
+def process_client_message(message):
+    SERVER_LOGGER.debug(f'Разбор сообщения от клиента : {message}')
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message and \
+            USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
         return {RESPONSE: 200}
     return {
-        RESPONSE: 400, ERROR: 'Bad Request'
+        RESPONSE: 400,
+        ERROR: 'Bad Request'
     }
 
 
+def create_arg_parser():
+    """
+    Парсер аргументов коммандной строки
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
+    parser.add_argument('-a', default='', nargs='?')
+    return parser
+
+
 def main():
-    try:
-        if '-p' in sys.argv:
-            listen_port = int(sys.argv[sys.argv.index('-p') + 1])
-        else:
-            listen_port = DEFAULT_PORT
-        if listen_port < 1024 or listen_port > 65535:
-            raise ValueError
-    except IndexError:
-        print('После параметра -\'p\' необходимо указать номер порта.')
-        sys.exit(1)
-    except ValueError:
-        print(
-            'В качастве порта может быть указано только число в диапазоне от 1024 до 65535.')
-        sys.exit(1)
+    parser = create_arg_parser()
+    namespace = parser.parse_args(sys.argv[1:])
+    listen_address = namespace.a
+    listen_port = namespace.p
 
-    try:
-        if '-a' in sys.argv:
-            addr = sys.argv[sys.argv.index('-a') + 1]
-        else:
-            addr = ''
-
-    except IndexError:
-        print(
-            'После параметра \'a\'- необходимо указать адрес, который будет слушать сервер.')
+    # проверка получения корретного номера порта для работы сервера.
+    if not 1023 < listen_port < 65536:
+        SERVER_LOGGER.critical(f'Попытка запуска сервера с указанием неподходящего порта '
+                               f'{listen_port}. Допустимы адреса с 1024 до 65535.')
         sys.exit(1)
+    SERVER_LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, '
+                       f'адрес с которого принимаются подключения: {listen_address}. '
+                       f'Если адрес не указан, принимаются соединения с любых адресов.')
+    # Готовим сокет
 
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    transport.bind((addr, listen_port))
+    transport.bind((listen_address, listen_port))
     transport.listen(MAX_CONNECTIONS)
 
     while True:
         client, client_address = transport.accept()
         try:
             message = get_message(client)
-            print(message)
-            response = message_response(message)
+            SERVER_LOGGER.debug(f'Получено сообщение {message}')
+            response = process_client_message(message)
+            SERVER_LOGGER.info(f'Cформирован ответ клиенту {response}')
             send_message(client, response)
+            SERVER_LOGGER.debug(f'Соединение с клиентом {client_address} закрывается.')
             client.close()
         except (ValueError, json.JSONDecodeError):
-            print('Принято некорретное сообщение от клиента.')
+            SERVER_LOGGER.error('Принято некорретное сообщение от клиента. Соединение закрыто.')
             client.close()
 
 
