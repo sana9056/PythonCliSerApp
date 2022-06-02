7cb0309ec2b84e8bd7cbabf3a71e01d38d977d6f
Sana9056 <sana9056@mail.ru>
2022-06-02 08:11:27 +0300

    HomeW4

--- Lesson3/common/variables.py
+++ Lesson3/common/variables.py
@@ -1,4 +1,3 @@
-"""Константы"""
 import logging
 
 # Порт поумолчанию для сетевого ваимодействия
@@ -14,7 +13,7 @@ ENCODING = 'utf-8'
 # Текущий уровень логирования
 LOGGING_LEVEL = logging.DEBUG
 # База данных для хранения данных сервера:
-SERVER_DATABASE = 'sqlite:///server_base.db3'
+SERVER_CONFIG = 'server.ini'
 
 # Прококол JIM основные ключи:
 ACTION = 'action'
@@ -31,12 +30,22 @@ ERROR = 'error'
 MESSAGE = 'message'
 MESSAGE_TEXT = 'mess_text'
 EXIT = 'exit'
+GET_CONTACTS = 'get_contacts'
+LIST_INFO = 'data_list'
+REMOVE_CONTACT = 'remove'
+ADD_CONTACT = 'add'
+USERS_REQUEST = 'get_users'
 
 # Словари - ответы:
 # 200
 RESPONSE_200 = {RESPONSE: 200}
+# 202
+RESPONSE_202 = {RESPONSE: 202,
+                LIST_INFO:None
+                }
 # 400
 RESPONSE_400 = {
             RESPONSE: 400,
             ERROR: None
         }
+
--- Lesson3/decos.py
+++ Lesson3/decos.py
@@ -1,27 +1,20 @@
-"""Декораторы"""
-
 import sys
+import logs.config_server_log
+import logs.config_client_log
 import logging
-import log.server_log_config
-import log.client_log_config
 
 # метод определения модуля, источника запуска.
-# Метод find () возвращает индекс первого вхождения искомой подстроки,
-# если он найден в данной строке.
-# Если его не найдено, - возвращает -1.
 if sys.argv[0].find('client') == -1:
-    # если не клиент то сервер!
-    LOGGER = logging.getLogger('server')
+    #если не клиент то сервер!
+    logger = logging.getLogger('server')
 else:
     # ну, раз не сервер, то клиент
-    LOGGER = logging.getLogger('client')
+    logger = logging.getLogger('client')
 
 
 def log(func_to_log):
-    """Функция-декоратор"""
-    def log_saver(*args, **kwargs):
-        ret = func_to_log(*args, **kwargs)
-        LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}. '
-                     f'Вызов из модуля {func_to_log.__module__}')
+    def log_saver(*args , **kwargs):
+        logger.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args} , {kwargs}. Вызов из модуля {func_to_log.__module__}')
+        ret = func_to_log(*args , **kwargs)
         return ret
     return log_saver
